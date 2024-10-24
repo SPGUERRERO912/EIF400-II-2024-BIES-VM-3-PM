fb47f51c6d655d9234ea2d47043624a4b11d161d
JoF00L <131736806+JoF00L@users.noreply.github.com>
2024-10-24 16:00:20 -0600

    Hotfix for testcase8

--- src/contextHandler.mjs
+++ src/contextHandler.mjs
@@ -1,37 +1,37 @@
 export class ContextHandler {
   constructor(vm) {
-    this.vm = vm
-    this.contextStack = []
-    this.parent = null
+    this.vm = vm;
+    this.contextStack = [];
+    this.parent = null;
   }
 
   saveContext() {
     const currentContext = {
       environment: this.vm.environment.map((env) => ({
         values: [...env], // Copia los valores del array
-        identifier: env.identifier // Copia el identifier de la pila
+        identifier: env.identifier, // Copia el identifier de la pila
       })),
       programCounter: this.vm.programCounter,
       callStack: [...this.vm.callStack],
-    }
-    this.contextStack.push(currentContext)
+    };
+    this.contextStack.push(currentContext);
   }
 
   restoreContext() {
     if (this.contextStack.length > 0) {
-      const restoredContext = this.contextStack.pop()
-      this.vm.register = [...this.vm.register]
+      const restoredContext = this.contextStack.pop();
+      this.vm.register = [...this.vm.register];
       this.vm.environment = restoredContext.environment
-        .map(env => {
-            const newEnv = [...env.values] // Restaura los valores de la pila interna
-            newEnv.identifier = env.identifier // Restaura el identifier
-            return newEnv
+        .map((env) => {
+          const newEnv = [...env.values]; // Restaura los valores de la pila interna
+          newEnv.identifier = env.identifier; // Restaura el identifier
+          return newEnv;
         })
-        .slice() 
-        .reverse()
-      this.vm.programCounter = restoredContext.programCounter
+        .slice()
+        .reverse();
+      this.vm.programCounter = restoredContext.programCounter;
     } else {
-      console.error('No context to restore!')
+      console.error('No context to restore!');
     }
   }
 }
--- src/contextLoader.mjs
+++ src/contextLoader.mjs
@@ -1,46 +1,53 @@
 export class ContextLoader {
   constructor(vm, contextHandler) {
-    this.vm = vm
-    this.contextHandler = contextHandler
+    this.vm = vm;
+    this.contextHandler = contextHandler;
   }
 
   loadFunction(functionName) {
-    const func = this.vm.functions[functionName]
+    const func = this.vm.functions[functionName];
     if (!func) {
-        console.error(`Function ${functionName} not found!`)
-        return
+      console.error(`Function ${functionName} not found!`);
+      return;
     }
-    const existingFunctionIndex = this.vm.environment.findIndex(env => env.identifier === functionName)
+    const existingFunctionIndex = this.vm.environment.findIndex(
+      (env) => env.identifier === functionName
+    );
     if (existingFunctionIndex !== -1) {
-        const existingFunction = this.vm.environment.splice(existingFunctionIndex, 1)[0]
-        this.vm.environment.unshift(existingFunction)
+      const existingFunction = this.vm.environment.splice(
+        existingFunctionIndex,
+        1
+      )[0];
+      this.vm.environment.unshift(existingFunction);
     } else {
-        this.args = this.vm.callStack.map((item) => item.args)[0]
-        this.registerCopy = [...this.vm.register]
-        this.locals = this.registerCopy.slice(-this.args).reverse()
-        func.instructions.isLoaded = true
-        this.newEnvironment = Array.from({ length: 10 }, (_, index) =>
-            this.args > 0 && this.locals[index] !== undefined
-                ? this.locals[index]
-                : []
-        )
-        this.newEnvironment.identifier = functionName
-        this.vm.environment.unshift(this.newEnvironment)
+      this.args = this.vm.callStack.map((item) => item.args)[0];
+      this.registerCopy = [...this.vm.register];
+      this.locals = this.registerCopy.slice(-this.args).reverse();
+      func.instructions.isLoaded = true;
+      this.newEnvironment = Array.from({ length: 10 }, (_, index) =>
+        this.args > 0 && this.locals[index] !== undefined
+          ? this.locals[index]
+          : []
+      );
+      this.newEnvironment.identifier = functionName;
+      this.vm.environment.unshift(this.newEnvironment);
     }
-    this.contextHandler.saveContext(functionName)
-    this.vm.register = this.registerCopy.slice(0, -this.args)
-    this.vm.programCounter = 0
-    this.vm.run(func.instructions)
-}
+    this.contextHandler.saveContext(functionName);
+    this.vm.register = this.registerCopy.slice(0, -this.args);
+    this.vm.programCounter = 0;
+    this.vm.run(func.instructions);
+  }
 
   returnFromFunction(functionName) {
-    const func = this.vm.functions[functionName]
-    this.contextHandler.restoreContext()
-    const stackIndex = this.vm.environment.findIndex(env => env.identifier === functionName )
+    const func = this.vm.functions[functionName];
+    this.contextHandler.restoreContext();
+    const stackIndex = this.vm.environment.findIndex(
+      (env) => env.identifier === functionName
+    );
     if (stackIndex !== -1) {
-      const [stack] = this.vm.environment.splice(stackIndex, 1)
-      this.vm.environment.unshift(stack)
+      const [stack] = this.vm.environment.splice(stackIndex, 1);
+      this.vm.environment.unshift(stack);
     }
-    this.vm.run(func.instructions)
+    this.vm.run(func.instructions);
   }
 }
